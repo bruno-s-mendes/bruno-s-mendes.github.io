9ccba096bdd224012821a821f2b239f06affe0aa
Bruno Mendes <mendessbruno@gmail.com>
2021-08-26 09:50:25 -0300

    finalizado versao mobile

--- script.js
+++ script.js
@@ -49,11 +49,69 @@ function email() {
   });
   }
 
+  function dropDown () {
+    const menuButton = document.getElementById('menu-button');
+    const dropdownList = document.getElementById('menu-mobile');
+    menuButton.addEventListener('click', () => {
+      (dropdownList.className === "hide")? dropdownList.className = 'show' : dropdownList.className = 'hide';
+    });
+  }
+
+  const inicioFuncMobile = () => {
+    const inicioBTN = document.getElementById('mobile-inicio-btn');
+    const inicioContainer = document.getElementById('inicio-container');
+    const menu = document.querySelector('.list');
+    menu.addEventListener('click', () => {
+      (inicioBTN.checked === true)? inicioContainer.style.display = 'block' : inicioContainer.style.display = 'none';
+    });
+  }
+  
+  const contatoFuncMobile = () => {
+    const contactBTN = document.getElementById('mobile-contato');
+    const contactContainer = document.getElementById('formulario');
+    const menu = document.querySelector('.list');
+    menu.addEventListener('click', () => {
+      (contactBTN.checked === true)? contactContainer.style.display = 'block' : contactContainer.style.display = 'none';
+    });
+  }
+  
+  const trybeFuncMobile = () => {
+    const contactBTN = document.getElementById('mobile-trybe');
+    const contactContainer = document.getElementById('trybe-works');
+    const menu = document.querySelector('.list');
+    menu.addEventListener('click', () => {
+      (contactBTN.checked === true)? contactContainer.style.display = 'block' : contactContainer.style.display = 'none';
+    });
+  }
+  
+  const workFuncMobile = () => {
+    const contactBTN = document.getElementById('mobile-empresa');
+    const contactContainer = document.getElementById('pro-works');
+    const menu = document.querySelector('.list');
+    menu.addEventListener('click', () => {
+      (contactBTN.checked === true)? contactContainer.style.display = 'block' : contactContainer.style.display = 'none';
+    });
+  }
+
+  function showAboutMe() {
+    const windowSize = window.screen.width;
+    const  aboutMe = document.getElementById('inicio-container');
+    if (windowSize < 600) {
+      aboutMe.className = 'show'
+    }
+  }
+
 window.onload = () => {
   inicioFunc();
   email();
   contatoFunc();
   trybeFunc();
   workFunc();
+  dropDown();
+  inicioFuncMobile();
+  contatoFuncMobile();
+  trybeFuncMobile();
+  workFuncMobile();
+  showAboutMe();
 };
 
